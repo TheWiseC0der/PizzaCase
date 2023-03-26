e951680148746db9ef5e40c3ceaa3a199c20cabf
C0deSn@ke <bryan.koolman@student.nhlstenden.com>
2023-03-26 18:32:15 +0200

    added Tuna

---
+++ Shared/Models/Pizza/PizzaTuna.cs
@@ -0,0 +1,16 @@
+﻿using DesignPatterns.Models.Bottom;
+using DesignPatterns.Models.Ingredient;
+
+namespace DesignPatterns.Models.Pizza
+{
+    class PizzaTuna : AmericanBottomPizza
+    {
+        public PizzaTuna()
+        {
+            this.Add(new TomatoSauce());
+            this.Add(new Cheese());
+            this.Add(new Tuna()); 
+        }
+
+    }
+}
