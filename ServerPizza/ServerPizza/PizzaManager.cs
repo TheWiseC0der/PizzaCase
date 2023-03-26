9d5f9dd75abc0c47a8af1a55f3776b5b3cc9adab
C0deSn@ke <bryan.koolman@student.nhlstenden.com>
2023-03-26 16:20:34 +0200

    added pizzamanager for managing orders related to connections
    thanks to the callback-functions in the IServer interface, the Pizzamanager can tie functionality to certain stages of the connection with the servers.

---
+++ ServerPizza/ServerPizza/PizzaManager.cs
@@ -0,0 +1,43 @@
+﻿using DesignPatterns.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerPizza
+{
+    public class PizzaManager
+    {
+        private IServer _iserver;
+        private Dictionary<string, Order> _orders;
+        public PizzaManager(IServer s)
+        {
+            _iserver = s;
+            s.OnClientConnect += OnClientConnect;
+            s.OnClientConnect += OnClientDisconnect;
+            s.OnClientRecieveMessage += OnCLientReceiveMessage;
+        }
+
+
+        public void OnClientConnect(string clientId)
+        {
+            Order? order;
+            _orders.TryGetValue(clientId, out order);
+            if (order == null)
+                _orders.Add(clientId, new Order());
+        }
+
+        public void OnClientDisconnect(string clientId)
+        {
+            _orders.Remove(clientId);
+        }
+
+        public void OnCLientReceiveMessage(string clientId, string message)
+        {
+            _orders.Remove(clientId);
+            _orders.Add(clientId, new Order());
+        }
+    }
+}
